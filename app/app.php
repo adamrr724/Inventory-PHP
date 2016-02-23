0965fdaeb38357b696a169def8f1bccc96005a7e
adam ross russell and jon sweeney <student@epicodus.com>
2016-02-22 16:52:11 -0800

    inventory project

--- app/app.php
+++ app/app.php
@@ -1,11 +1,10 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."/../src/Task.php";
-    require_once __DIR__."/../src/Category.php";
+    require_once __DIR__."/../src/Item.php";
 
     $app = new Silex\Application();
-    
-    $server = 'mysql:host=localhost;dbname=to_do';
+
+    $server = 'mysql:host=localhost;dbname=inventory';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
@@ -18,33 +17,18 @@
         return $app['twig']->render('index.html.twig');
     });
 
-    $app->get("/tasks", function() use ($app) {
-        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
+    $app->get("/items", function() use ($app) {
+        return $app['twig']->render('inventory.html.twig', array('items' => Item::getAll()));
     });
 
-    $app->get("/categories", function() use ($app) {
-        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    $app->post("/items", function() use ($app) {
+        $item = new Item($_POST['name'], $_POST['description'], $_POST['value_amount'], $_POST['currency_type'], $_POST['collectable_type']);
+        $item->save();
+        return $app['twig']->render('inventory.html.twig', array('items' => Item::getAll()));
     });
 
-    $app->post("/tasks", function() use ($app) {
-        $task = new Task($_POST['description']);
-        $task->save();
-        return $app['twig']->render('tasks.html.twig', array('tasks' => Task::getAll()));
-    });
-
-    $app->post("/delete_tasks", function() use ($app) {
-        Task::deleteAll();
-        return $app['twig']->render('index.html.twig');
-    });
-
-    $app->post("/categories", function() use ($app) {
-        $category = new Category($_POST['name']);
-        $category->save();
-        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
-    });
-
-    $app->post("/delete_categories", function() use ($app) {
-        Category::deleteAll();
+    $app->post("/delete_items", function() use ($app) {
+        Item::deleteAll();
         return $app['twig']->render('index.html.twig');
     });
 
